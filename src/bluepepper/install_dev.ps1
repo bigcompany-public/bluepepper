9dd25608b57482ce12f03b5802ac2bbec872da5c
Tristan Languebien <tristan.languebien@bigcompany.fr>
2026-04-16 13:46:24 +0200

    add reset option

--- src/bluepepper/install_dev.ps1
+++ src/bluepepper/install_dev.ps1
@@ -9,7 +9,7 @@ if (-Not (Test-Path $editable_mode_file)) {
 
 # Install virtual environment & dependencies
 $install_script = Join-Path -Path $PSScriptRoot -ChildPath "install/install.py"
-& python $install_script --update_packages
+& python $install_script --reset --update_packages
 
 if ($LASTEXITCODE -ne 0) {
     throw "Python installation script failed with exit code $LASTEXITCODE"
