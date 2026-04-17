4ed9888ec69c077fec383473e1dd1ed09146c56f
Tristan Languebien <tristan.languebien@bigcompany.fr>
2026-04-17 14:07:28 +0200

    linting & type annotations

--- bluepepper/credentials.py
+++ bluepepper/credentials.py
@@ -1,12 +1,15 @@
 import argparse
-import keyring
 import logging
 from typing import Tuple
-from ldap3 import Connection, AUTO_BIND_NO_TLS
-from ldap3.core.exceptions import LDAPException, LDAPBindError
+
+import keyring
+from ldap3 import AUTO_BIND_NO_TLS, Connection
+from ldap3.core.exceptions import LDAPBindError, LDAPException
+
 from conf.domain import DomainConfig
 
-def test_credentials(user:str, password:str) -> bool:
+
+def test_credentials(user: str, password: str) -> bool:
     """Returns True if the users credentials are valid, False otherwise"""
     domain = DomainConfig.ldap_server
     user = user.split("@")[0].split("\\")[-1]
@@ -17,21 +20,23 @@ def test_credentials(user:str, password:str) -> bool:
     except (LDAPBindError, LDAPException):
         return False
 
-def save_credentials(service:str, user:str, password:str):
+
+def save_credentials(service: str, user: str, password: str):
     """
     Saves the credentials to Windows Credential Manager
     """
     if not test_credentials(user=user, password=password):
         raise LDAPBindError("Invalid credentials")
-    logging.info(f"Saving credentials for service {service} : {user}//{'*'*len(password)}")
+    logging.info(f"Saving credentials for service {service} : {user}//{'*' * len(password)}")
     keyring.set_password(service_name=service, username=user, password=password)
-    
-def get_saved_credentials(service:str) -> Tuple:
+
+
+def get_saved_credentials(service: str) -> Tuple[str, str] | None:
     """Gets the credentials from windows credentials manager"""
     cred = keyring.get_credential(service_name=service, username=None)
     if cred:
         return (cred.username, cred.password)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -40,5 +45,3 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--service", required=False)
     args = parser.parse_args()
     # cred = get_saved_credentials(service=args.service)
-    # print(cred)
-    print(test_credentials("tristan.languebien", 'cZbu7Wbh'))
