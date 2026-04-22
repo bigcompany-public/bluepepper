64f33fb7fb3dc54d73dd7e0f0a347b0bc8a8fa38
Tristan Languebien <tlanguebien@gmail.com>
2026-04-22 13:42:19 +0200

    add context menu

--- bluepepper/gui/widgets/lab_widget/labwidget.py
+++ bluepepper/gui/widgets/lab_widget/labwidget.py
@@ -3,6 +3,9 @@ This module contains the LabWidget widget, used as a main page to display
 and test every widget our tools use.
 """
 
+from qtpy.QtWidgets import QMenu, QWidget
+
+from bluepepper.gui.utils import stylesheet
 from bluepepper.gui.widgets.container import (
     ContainerDialog,
     ContainerWidget,
@@ -10,7 +13,6 @@ from bluepepper.gui.widgets.container import (
     qtawesome,
 )
 from bluepepper.gui.widgets.lab_widget.ui_labwidget import Ui_LabWidget
-from qtpy.QtWidgets import QWidget
 
 
 class LabWidget(QWidget):
@@ -18,6 +20,23 @@ class LabWidget(QWidget):
         super().__init__()
         ui = Ui_LabWidget()
         ui.setupUi(self)
+
+    def contextMenuEvent(self, event):
+        """
+        This method pops a Qmenu widget when the user right clicks on the table
+        """
+        menu = QMenu()
+        menu.setStyleSheet(stylesheet)
+        menu.addAction("Action1")
+        menu.addAction("Action2 with a long name")
+        action = menu.addAction("Action 3 with icon")
+        icon = qtawesome.icon(
+            "ei.adjust",
+            scale_factor=1.1,
+            color="#DDDDDD",
+        )
+        action.setIcon(icon)
+        menu.exec_(event.globalPos())
 
 
 if __name__ == "__main__":
