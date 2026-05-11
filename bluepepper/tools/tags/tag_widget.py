016a06a5d21495c7193b3e4a26243c6b59876976
Tristan Languebien <tlanguebien@gmail.com>
2026-05-11 12:09:17 +0200

    remove test code

--- bluepepper/tools/tags/tag_widget.py
+++ bluepepper/tools/tags/tag_widget.py
@@ -1,5 +1,3 @@
-from argparse import ArgumentParser
-
 import qtawesome
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
@@ -8,10 +6,6 @@ from PySide6.QtWidgets import (
     QLabel,
     QSizePolicy,
 )
-
-from bluepepper.core import database
-from bluepepper.gui.utils import get_qta_icon
-from bluepepper.gui.widgets.container import ContainerWidget, get_qt_app
 
 
 class TagWidget(QFrame):
@@ -28,14 +22,14 @@ class TagWidget(QFrame):
         self.update_preview()
 
     def setup_ui(self):
-        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         self.setFixedHeight(self._size)
         layout = QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(6)
         self.icon_label = QLabel()
         self.text_label = QLabel(self._tag)
-        self.text_label.setAlignment(Qt.AlignCenter)
+        self.text_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.icon_label)
         layout.addWidget(self.text_label)
         self.setLayout(layout)
@@ -81,34 +75,3 @@ class TagWidget(QFrame):
             background-color: none;
             """
         )
-
-
-def show_asset_tag(document_id):
-    document = database.get_asset_tag_document_by_id(document_id)
-    app = get_qt_app()
-    icon = get_qta_icon(name="mdi.tag-text", scale_factor=1.25)
-    from qtpy.QtWidgets import QLabel, QTableWidget, QTableWidgetItem
-
-    table = QTableWidget()
-    table.setColumnCount(1)
-    table.insertRow(0)
-    item = QTableWidgetItem("Jaj")
-    # item.setText("JAJ")
-    l = QLabel()
-    table.setItem(0, 0, item)
-    widget = TagWidget(document)
-    table.setCellWidget(0, 0, widget)
-    # table.setItem(0, 0, item)
-    container = ContainerWidget(widget=table, icon=icon, title="Tag")
-    container.show()
-    app.exec()
-
-
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("-aid", "--asset_id", required=False)
-    parser.add_argument("-sid", "--shot_id", required=False)
-    args = parser.parse_args()
-
-    if args.asset_id:
-        print(show_asset_tag(args.asset_id))
