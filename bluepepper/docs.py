9c2873f46e43e5da824fa29cde33e31df097ea8f
Tristan Languebien <tlanguebien@gmail.com>
2026-05-15 12:44:51 +0200

    change link to documentation

--- bluepepper/docs.py
+++ bluepepper/docs.py
@@ -2,4 +2,4 @@ import webbrowser
 
 
 def open_documentation():
-    webbrowser.open(url="https://github.com/bigcompany-public/bluepepper?tab=readme-ov-file#table-of-contents")
+    webbrowser.open(url="https://bigcompany-public.github.io/bluepepper/")
