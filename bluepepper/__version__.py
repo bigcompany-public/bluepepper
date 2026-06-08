d05d25ed5bc35bd4190646e042fa947dfba41dd6
Tristan Languebien <tlanguebien@gmail.com>
2026-06-08 13:30:31 +0200

    reintroduce bump2version for easier tag creation

--- bluepepper/__version__.py
+++ bluepepper/__version__.py
@@ -1,1 +1,8 @@
-__version__ = "0.0.1"
+from pathlib import Path
+
+import toml
+
+pyproject_path = Path(__file__).parent.parent.joinpath("pyproject.toml")
+pyproject = toml.loads(pyproject_path.read_text())
+
+__version__ = pyproject["project"]["version"]
